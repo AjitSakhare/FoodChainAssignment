b898509152f123d3381b8eebf126acee70a696cb
Ajit Sakhare <tech.ajitsakhare@gmail.com>
2020-04-05 19:01:45 +0530

    Customize and total udpateed

--- FoodChain/wwwroot/js/Home.js
+++ FoodChain/wwwroot/js/Home.js
@@ -1,46 +1,93 @@
 ﻿$(document).ready(function () {
-    //Pizza
-    $("#BtnOnePizza").click(function () {
-        $("#BtnOnePizza").hide();
-        $("#plusMinusPrdOne").show();
-    })
-    $("#BtnTwoPizza").click(function () {
-        $("#BtnTwoPizza").hide();
-        $("#plusMinusPrdTwo").show();
+    var finalAmountGlobe = parseInt($("#tatalPrice").text());
+    $(function () {
+        $("a[class='Update']").click(function () {
+            $("#MyPopup").modal("show");
+            return false;
+        });
+    });
 
-    })
-    $("#BtnThreePizza").click(function () {
-        $("#BtnThreePizza").hide();
-        $("#plusMinusPrdThree").show();
-    })
-
-    //sandwiches
-    $("#BtnOnesandwich").click(function () {
-        $("#BtnOnesandwich").hide(); 
-        $("#plusMinusPrdOnesandwich").show();
-    })
-    $("#BtnTwosandwich").click(function () {
-        $("#BtnTwosandwich").hide();
-        $("#plusMinusPrdTwosandwich").show();
-
-    })
-    $("#BtnThreesandwich").click(function () {
-        $("#BtnThreesandwich").hide();
-        $("#plusMinusPrdThreesandwich").show();
+    $('.increasePrice').on('click', function () {
+        var count = parseInt($(this).siblings('.txtCartCount:first').val());
+        count = count + 1;
+        $(this).siblings('.txtCartCount:first').val(count);
+        var price = parseInt($(this).parent().parent().parent().find('.lblPrice').text());
+        var finalAmount = parseInt($("#tatalPrice").text());
+        finalAmount = finalAmount + price;
+        $("#tatalPrice").text(finalAmount)
+        
     })
 
-    //Drinks
-    $("#BtnOneDrinks").click(function () {
-        $("#BtnOneDrinks").hide();
-        $("#plusMinusPrdOneDrinks").show();
+    $('.decresePrice').on('click', function () {
+        var count = parseInt($(this).siblings('.txtCartCount:first').val());
+        count = count - 1;
+        if (count > 0) {
+            $(this).siblings('.txtCartCount:first').val(count);
+            var price = parseInt($(this).parent().parent().parent().find('.lblPrice').text());
+            var finalAmount = parseInt($("#tatalPrice").text());
+            finalAmount = finalAmount - price;
+            $("#tatalPrice").text(finalAmount)
+        }
+       
+        
     })
-    $("#BtnTwoDrinks").click(function () {
-        $("#BtnTwoDrinks").hide();
-        $("#plusMinusPrdTwoDrinks").show();
+    $('.glyphicon-trash').on('click', function () {
+        $(this).parent().hide()
+        $(this).parent().parent().find('button').show()
+        var count = parseInt($(this).siblings('.txtCartCount:first').val());
+        $(this).siblings('.txtCartCount:first').val(1);
+        var price = parseInt($(this).parent().parent().parent().find('.lblPrice').text());
+        var totalPrice = parseInt($("#tatalPrice").text())
+        var finalAmout = parseInt(totalPrice) - (parseInt(price) * parseInt(count));
+        $("#tatalPrice").text(finalAmout);
+        
+    })
+
+    $('.AddToCard').on('click', function () {
+        var price = parseInt($(this).parent().parent().find('.lblPrice').text());
+        var totalPrice = parseInt( $("#tatalPrice").text());
+        totalPrice = parseInt(totalPrice) + parseInt(price);
+        $("#tatalPrice").text(totalPrice)
+        $(this).hide()
+        $(this).next('div').show();
 
     })
-    $("#BtnThreeDrinks").click(function () {
-        $("#BtnThreeDrinks").hide();
-        $("#plusMinusPrdThreeDrinks").show();
+
+
+
+    $('.textExtraBredPlus').on('click', function () {
+        var count = parseInt($(this).siblings('.txtCartCount:first').val());
+        count = count + 10;
+        $(this).siblings('.txtCartCount:first').val(count);  
     })
+
+
+    $('.textExtraBredMinus').on('click', function () {
+        var count = parseInt($(this).siblings('.txtCartCount:first').val());
+        count = count - 10;
+        if(count>=20)
+        $(this).siblings('.txtCartCount:first').val(count);
+    })
+
+
+    $('.textExtraToppPlus').on('click', function () {
+        var count = parseInt($(this).siblings('.txtCartCount:first').val());
+        count = count + 20;
+        $(this).siblings('.txtCartCount:first').val(count);
+    })
+
+
+    $('.textExtraToppMinus').on('click', function () {
+        var count = parseInt($(this).siblings('.txtCartCount:first').val());
+        count = count - 20;
+        if (count >= 20)
+            $(this).siblings('.txtCartCount:first').val(count);
+    })
+
+
+
+
+
+
+
 });
